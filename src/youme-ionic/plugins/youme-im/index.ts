10b2263bfbf25dc911319a217a6b2de763a852d0
李繁 <fan.lion@foxmail.com>
2019-08-09 22:39:31 +0800

    完成callback类型的函数包装方式

--- src/youme-ionic/plugins/youme-im/index.ts
+++ src/youme-ionic/plugins/youme-im/index.ts
@@ -25,10 +25,10 @@ import { Observable } from 'rxjs';
  */
 @Plugin({
   pluginName: 'YoumeIM',
-  plugin: 'https://github.com/youmesdk/YoumeIMCordovaPlugin.git', // npm package name, example: cordova-plugin-camera
+  plugin: 'cordova-plugin-youme-im', // npm package name, example: cordova-plugin-camera
   pluginRef: 'cordova.plugins.YoumeIMCordovaPlugin', // the variable reference to call the plugin, example: navigator.geolocation
   repo: 'https://github.com/youmesdk/YoumeIMCordovaPlugin', // the github repository URL for the plugin
-  install: 'cordova plugin add YoumeIMCordovaPlugin', // OPTIONAL install command, in case the plugin requires variables
+  install: 'cordova plugin add cordova-plugin-youme-im', // OPTIONAL install command, in case the plugin requires variables
   installVariables: [], // OPTIONAL the plugin requires variables
   platforms: ['Android'] // Array of platforms supported, example: ['Android', 'iOS']
 })
@@ -38,33 +38,31 @@ export class YoumeIM extends IonicNativePlugin {
   /**
    * init sdk
    * 
-   * @param appKey {string} 
-   * @param secretKey {number} Another param to configure something
-   * @return {Promise<any>} Returns a promise that resolves when something happens
+   * @param {string} appKey 
+   * @param {number} secretKey - Another param to configure something
+   * @return {Promise<any>} - Returns a promise that resolves when something happens
    * @memberof YoumeIM
    */
   @Cordova()
   init(appKey: string, secretKey: string, regionId: string): Promise<any> { return; }
 
   /**
-   * 
    * login
    * 
-   * @param userid {string} 
-   * @param password {string}
-   * @param token {string}
-   * @return {Promise<any>} Returns a promise
+   * @param {string} userid 
+   * @param {string} password
+   * @param {string} token
+   * @return {Promise<any>} - Returns a promise
    * @memberof YoumeIM
    */
   @Cordova()
   login(userid: string, password: string, token: string): Promise<any> { return; }
 
   /**
-   *
    * logout sdk
    *
    * @memberof YoumeIM
-   * @return {Promise<any>} Returns Promise
+   * @return {Promise<any>} - Returns Promise
    * @memberof YoumeIM
    */
   @Cordova()
@@ -73,92 +71,94 @@ export class YoumeIM extends IonicNativePlugin {
   /**
    * send text message
    *
-   * @param strRecvId {string}
-   * @param iChatType {number} 
-   * @param strMsgContent {string} 
-   * @param strAttachParam  {string} 
-   * @returns {Promise<any>}
-   * @return {Promise<any>} Returns Promise
+   * @param {string} strRecvId
+   * @param {number} iChatType
+   * @param {string} strMsgContent
+   * @param {string} strAttachParam
+   * @return {Promise<any>} - Returns Promise
    * @memberof YoumeIM
    */
   @Cordova()
   sendTextMessage(strRecvId: string, iChatType: number, strMsgContent: string, strAttachParam: string): Promise<any> { return; }
   
   /**
-   * regist callback for reconnect event
+   * regist callback function for reconnect event
    * 
+   * @param {Function} success - callback function for success
+   * @param {Function} error - callback function for error
    * @memberof YoumeIM
    */
-  @Cordova()
-  registerReconnectCallback() { return; }
+  @Cordova({ sync: true })
+  registerReconnectCallback(success: Function, error: Function): void { return; }
 
   /**
-   * regist callback for kick out event
-   *
+   * regist callback function for kick out event
+   * 
+   * @param {Funtion} success - callback function for success
+   * @param {Function} error - callback function for error
    * @memberof YoumeIM
    * @return
    */
   @Cordova()
-  registerKickOffCallback() { return; }
+  registerKickOffCallback(success: Function, error: Function): void { return; }
 
   /**
-   * 
    * start record audio message
-   * 
+   *
+   * @param {string} recvID
+   * @param {number} chatType
+   * @param {string} extraText
+   * @param {boolean} needRecognize
+   * @returns {Promise<any>}
    * @memberof YoumeIM
-   * @param recvID {string}
-   * @param chatType {number}
-   * @param extraText {string}
-   * @param needRecognize {boolean}
-   * @return {Promise<any>} Returns Promise
    */
   @Cordova()
   startRecordAudioMessage(recvID: string, chatType: number, extraText: string, needRecognize: boolean): Promise<any> { return; }
 
   /**
    * cancel audio message
-   * 
+   *
+   * @returns {Promise<any>}
    * @memberof YoumeIM
-   * @return {Promise<any>} Returns Promise
    */
   @Cordova()
   cancelAudioMessage(): Promise<any> { return; }
 
   /**
    * stop record audio message, and send it
-   * 
+   *
+   * @returns {Promise<any>}
    * @memberof YoumeIM
-   * @return {Promise<any>} Returns Promise
    */
   @Cordova()
   stopAndSendAudioMessage(): Promise<any> { return; }
 
   /**
    * join single chart room by room id
-   * 
+   *
+   * @param {string} roomID
+   * @returns {Promise<any>}
    * @memberof YoumeIM
-   * @param roomID {string} room id
-   * @return {Promise<any>} Returns Promise
    */
   @Cordova()
   joinChatRoom(roomID: string): Promise<any> { return; }
 
   /**
    * leave chat room by id
-   * 
+   *
+   * @param {string} roomID
+   * @returns {Promise<any>}
    * @memberof YoumeIM
-   * @param roomID {string} roomt id
-   * @return {Promise<any>} Returns Promise
    */
   @Cordova()
   leaveChatRoom(roomID: string): Promise<any> { return; }
 
   /**
    * start play audio
-   * 
+   *
+   * @param {string} audioPath
+   * @returns {Promise<any>}
    * @memberof YoumeIM
-   * @param audioPath {string} audio path
-   * @return {Promise<any>} Returns Promise
    */
   @Cordova()
   startPlayAudio(audioPath: string): Promise<any> { return; }
@@ -166,8 +166,8 @@ export class YoumeIM extends IonicNativePlugin {
   /**
    * stop play audio
    * 
-   * @memberof YoumeIM
    * @return {Promise<any>} Returns Promise
+   * @memberof YoumeIM
    */
   @Cordova()
   stopPlayAudio(): Promise<any> { return; }
